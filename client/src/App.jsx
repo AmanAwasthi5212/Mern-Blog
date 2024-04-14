ad4a271bb37002bcb1bf4141e51b11801aca6235
AmanAwasthi5212 <202212077@daiict.ac.in>
2024-04-14 20:56:27 +0530

    Header Section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,12 @@ import Dashboard from './pages/Dashboard'
 import About from './pages/About'
 import Home from './pages/Home'
 import Projects from './pages/Projects'
+import Header from './Components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
