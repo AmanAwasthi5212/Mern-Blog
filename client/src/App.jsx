a5e0c1359d383f5b930d87077234a7e42e8e19eb
AmanAwasthi5212 <202212077@daiict.ac.in>
2024-04-21 01:03:23 +0530

    added update post functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Signin from './pages/Signin'
-import SignUp from './pages/SignUp'
-import Dashboard from './pages/Dashboard'
-import About from './pages/About'
-import Home from './pages/Home'
-import Projects from './pages/Projects'
-import Header from './Components/Header'
-import Footer from './Components/Footer'
-import PrivateRoute from './Components/PrivateRoute'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/Signin';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import SignUp from './pages/SignUp';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import PrivateRoute from './Components/PrivateRoute';
 import OnlyAdminPrivateRoute from './Components/OnlyAdminPrivateRoute';
 import CreatePost from './pages/CreatePost';
+import UpdatePost from './pages/UpdatePost';
 
 export default function App() {
   return (
@@ -19,14 +19,16 @@ export default function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
-        <Route path='/sign-in' element={<Signin />} />
+        <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
         <Route element={<PrivateRoute />}>
           <Route path='/dashboard' element={<Dashboard />} />
         </Route>
         <Route element={<OnlyAdminPrivateRoute />}>
           <Route path='/create-post' element={<CreatePost />} />
+          <Route path='/update-post/:postId' element={<UpdatePost />} />
         </Route>
+
         <Route path='/projects' element={<Projects />} />
       </Routes>
       <Footer />
